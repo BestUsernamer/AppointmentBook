1ed98d783fa4c1526f9b705ae3f00d4ff21bd5c0
BestUsernamer <haoz8810@nycstudents.net>
2025-10-28 13:19:49 -0400

    Added stuff

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,80 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Main {
+    public static int read() throws FileNotFoundException {
+        int meetings = 0;
+        File f = new File("Schedules.txt");
+        Scanner s = new Scanner(f);
+        String schedule ="";
+        while (s.hasNext()) {
+            while (s.hasNextBoolean()) {
+    schedule += s.nextBoolean() + " ";
+                AppointmentBook a = new AppointmentBook(readSchedule(schedule));
 
-    public static void main(String[] args) {
+                if (a.makeAppointment(s.nextInt(), s.nextInt(), s.nextInt()))
+                    meetings++;
+            }
 
-        boolean[][] schedule = new boolean[8][60];
-        AppointmentBook a = new AppointmentBook(schedule);
-        //test cases 1
-        for(int i = 10; i < 15; i++) schedule [2][i] = true;
-        for(int i = 30; i < 45; i++) schedule [2][i] = true;
-        for(int i = 50; i < 60; i++) schedule [2][i] = true;
-        a.printPeriod(2);
-
-
-
-        //test cases 2
-
-        AppointmentBook b = new AppointmentBook(schedule);
-        for(int i = 25; i < 30; i++) schedule [1][i] = true;
-        for(int i = 0; i < 15; i++) schedule [2][i] = true;
-        for(int i = 41; i < 60; i++) schedule [2][i] = true;
-        for(int i = 5; i < 30; i++) schedule [3][i] = true;
-        for(int i = 44; i < 60; i++) schedule [3][i] = true;
-        int period = 2;
-        while   (period <5) {
-            System.out.println("Period: " + period);
-            b.printPeriod(period);
-            period++;
         }
+        schedule = "";
+        return meetings;
+    }
+public static boolean[][] readSchedule(String lines) {
+        Scanner s = new Scanner(lines);
+        boolean [][] schedule = new boolean[8][60];
+        for (int i = 0; i < schedule.length; i++) {
+            for (int j = 0; j < schedule [i].length; j++) {
+                schedule[i][j] = s.nextBoolean();
+            }
+        }
+        return schedule;
+}
 
-            //test cases 3
-
-            AppointmentBook c = new AppointmentBook(schedule);
-            for(int i = 25; i < 30; i++) schedule [2][i] = true;
-            for(int i = 3; i < 15; i++) schedule [3][i] = true;
-            for(int i = 41; i < 60; i++) schedule [3][i] = true;
-            for(int i = 27; i < 29; i++) schedule [4][i] = true;
-            for(int i = 44; i < 60; i++) schedule [4][i] = true;
-            c.printPeriod(period);
+    public static void main(String[] args) throws IOException {
+System.out.println(read());
 
 
-        System.out.println(c.makeAppointment(2, 4, 22));
+//        boolean[][] schedule = new boolean[8][60];
+//        AppointmentBook a = new AppointmentBook(schedule);
+//        //test cases 1
+//        for(int i = 10; i < 15; i++) schedule [2][i] = true;
+//        for(int i = 30; i < 45; i++) schedule [2][i] = true;
+//        for(int i = 50; i < 60; i++) schedule [2][i] = true;
+//        a.printPeriod(2);
+//
+//
+//
+//        //test cases 2
+//
+//        AppointmentBook b = new AppointmentBook(schedule);
+//        for(int i = 25; i < 30; i++) schedule [1][i] = true;
+//        for(int i = 0; i < 15; i++) schedule [2][i] = true;
+//        for(int i = 41; i < 60; i++) schedule [2][i] = true;
+//        for(int i = 5; i < 30; i++) schedule [3][i] = true;
+//        for(int i = 44; i < 60; i++) schedule [3][i] = true;
+//        int period = 2;
+//        while   (period <5) {
+//            System.out.println("Period: " + period);
+//            b.printPeriod(period);
+//            period++;
+//        }
+//
+//            //test cases 3
+//
+//            AppointmentBook c = new AppointmentBook(schedule);
+//            for(int i = 25; i < 30; i++) schedule [2][i] = true;
+//            for(int i = 3; i < 15; i++) schedule [3][i] = true;
+//            for(int i = 41; i < 60; i++) schedule [3][i] = true;
+//            for(int i = 27; i < 29; i++) schedule [4][i] = true;
+//            for(int i = 44; i < 60; i++) schedule [4][i] = true;
+//            c.printPeriod(period);
+//
+//
+//        System.out.println(c.makeAppointment(2, 4, 22));
 
     }
 
